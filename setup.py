63e75a55116851cc50d69d10c2652c826a943efd
RalfG <ralfg@hotmail.be>
2019-12-17 20:42:15 +0100

    Initial code commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	name='hello_py',
+	version='0.1.0',
+	license='apache-2.0',
+	author='Ralf Gabriels',
+	author_email='ralfg@hotmail.be',
+	packages=['hello_py'],
+	include_package_data=False,
+	entry_points={'console_scripts': ['hello_py=hello_py.__main__:main']},
+	install_requires=[],
+	python_requires='>=3.6,<4',
+)
